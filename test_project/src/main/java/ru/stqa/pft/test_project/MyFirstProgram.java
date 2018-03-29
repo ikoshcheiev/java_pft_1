48e5f172d16f8ae0e717abbe2301777c9bd0da5c
ikoshcheiev <koshcheiev.vanya@gmail.com>
2018-03-29 22:01:39 +0300

    New change

--- test_project/src/main/java/ru/stqa/pft/test_project/MyFirstProgram.java
+++ test_project/src/main/java/ru/stqa/pft/test_project/MyFirstProgram.java
@@ -1,13 +1,20 @@
 package ru.stqa.pft.test_project;
 
 public class MyFirstProgram {
-  public static void main(String[] args) {
-    hello("world");
-    hello("user");
-    hello("Alexei");
-  }
+    public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        hello("Alexei");
 
-  private static void hello(String somebody) {
-    System.out.println("Hello " + somebody + "!");
-  }
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    }
+
+    private static void hello(String somebody) {
+        System.out.println("Hello " + somebody + "!");
+    }
+
+    public static double area(double l){
+        return l * l;
+    }
 }
